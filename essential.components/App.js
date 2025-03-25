e06a2e8f7b56f4a93f09b241de79156a763811b0
Enzo Alves Turcovic <enzoturcovic@gmail.com>
2025-03-25 14:16:42 -0300

    Finish Project

--- essential.components/App.js
+++ essential.components/App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Image} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.header}>Hello World!</Text>
+      <Image source={require("./assets/img.png")} style={styles.image}/>
+      <Text style={styles.text}>RJ and Hammy</Text>
     </View>
   );
 }
@@ -16,5 +17,30 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  header: {
+    backgroundColor: 'green',
+    fontSize: 70,
+    fontWeight: 'bold',
+    color: 'white',
+    width: '100%',
+    height: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+  },
+
+  image: {
+    width: 500,
+    height: 500,
+    borderRadius: 25,
+    margin: 20,
+  },
+
+  text: {
+    fontSize: 50,
+    color: 'green',
+    fontWeight: 'bold',
   },
 });
